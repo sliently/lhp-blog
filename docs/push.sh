ef98492b07676bd820c00ac8118090798b81ecf0
xthp <847186328@qq.com>
2018-08-16 19:54:06 +0800

    shell

---
+++ docs/push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "开始上传"
+rm -rf docs/
+cp -r _book docs/
+echo `请输入commit信息`
+read msg
+git add -A
+git commit -m $msg
+git fetch
+git rebase origin/master
+git push origin master:master
